9cea7ff1c4caf2639b53681206d610ceafef8f25
Tania Dimitrova <tania_mtv@mail.bg>
2017-05-06 13:11:50 +0300

    Side-bar completely done

--- js/sidebar-javascript.js
+++ js/sidebar-javascript.js
@@ -3,10 +3,15 @@
 $(document).ready(function () {
     var trigger = $('.hamburger'),
         overlay = $('.overlay'),
+        nav = $('.li-sidebar'),
         isClosed = false;
 
     trigger.click(function () {
         hamburger_cross();
+    });
+    nav.click(function (ev) {
+        //var target = this.target;
+        hideNav();
     });
 
     function hamburger_cross() {
@@ -16,17 +21,31 @@ $(document).ready(function () {
             trigger.removeClass('is-open');
             trigger.addClass('is-closed');
             isClosed = false;
-        } else {
+        }
+        else {
             overlay.show();
             trigger.removeClass('is-closed');
             trigger.addClass('is-open');
             isClosed = true;
         }
+
     }
+    function hideNav() {
+        trigger.removeClass('is-open');
+        trigger.addClass('is-closed');
+         isClosed = true;
+        overlay.hide();
+    }
+
 
     $('[data-toggle="offcanvas"]').click(function () {
         $('#wrapper').toggleClass('toggled');
     });
+    nav.click(function () {
+        if( $('#wrapper').hasClass('toggled')){
+            $('#wrapper').removeClass('toggled');
+        }
+    })
 
 
 });
