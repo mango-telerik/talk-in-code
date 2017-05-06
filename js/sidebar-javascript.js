cbfe7722af3b4a7d3eafa4394f277a471a225822
Tania Dimitrova <tania_mtv@mail.bg>
2017-05-06 16:48:40 +0300

    Add files via upload

--- js/sidebar-javascript.js
+++ js/sidebar-javascript.js
@@ -9,10 +9,7 @@ $(document).ready(function () {
     trigger.click(function () {
         hamburger_cross();
     });
-    nav.click(function (ev) {
-        //var target = this.target;
-        hideNav();
-    });
+
 
     function hamburger_cross() {
 
@@ -30,18 +27,17 @@ $(document).ready(function () {
         }
 
     }
-    function hideNav() {
-        trigger.removeClass('is-open');
-        trigger.addClass('is-closed');
-         isClosed = true;
-        overlay.hide();
-    }
+
 
 
     $('[data-toggle="offcanvas"]').click(function () {
         $('#wrapper').toggleClass('toggled');
     });
     nav.click(function () {
+        overlay.hide();
+        trigger.removeClass('is-open');
+        trigger.addClass('is-closed');
+        isClosed = false;
         if( $('#wrapper').hasClass('toggled')){
             $('#wrapper').removeClass('toggled');
         }
